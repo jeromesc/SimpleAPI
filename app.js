e66611fbc5af0642314e8619a71277d4efdea928
Jerome Schmaltz <Jerome@MacBook-Pro-de-Jerome-2.local>
2016-01-15 12:55:17 -0500

    update CORS 1

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+ // res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.');
+res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -24,7 +25,7 @@ app.use(function (req, res, next) {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  //res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
 
